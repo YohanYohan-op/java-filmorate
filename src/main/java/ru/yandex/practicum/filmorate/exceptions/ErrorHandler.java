89dba645ce644d1ae4d28b67b8685b3eccb4e1b4
Слава <korolslava0@gmail.com>
2025-03-22 10:17:21 +0700

    add new functional(like, dislike, addFriend an more)

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -23,7 +24,7 @@ public class ErrorHandler {
         return new ErrorResponse("Validation Exception", "Wrong information in the request");
     }
 
-    @ExceptionHandler
+    @ExceptionHandler({ValidationException.class, IllegalArgumentException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse jsonHandle(final ValidationException e) {
         return new ErrorResponse("Validation Exception", "Wrong information in the request");
@@ -33,5 +34,11 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse parsHandle(final NumberFormatException e) {
         return new ErrorResponse("Path error", "invalid path variable");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred: " + ex.getMessage());
     }
 }
